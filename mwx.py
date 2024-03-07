cdf7a28ed6ac66ed6df5b638a2a534944e10969c
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-03-07 09:21:32 +0300

    Приведение заголовков функций к PEP8

--- mwx.py
+++ mwx.py
@@ -2,7 +2,13 @@
 import os
 
 
-def fio(str_fio: str, order='fio') -> str:
+def fio(str_fio: str, order: str = 'fio') -> str:
+    """
+    Получить из полного ФИО фамилию и инициалы.
+    Параметры:
+        str_fio - строка полные фамилия и мя и отчество
+        order - строка парядок выедения (fio - фамилия и иницыалы, iof - иницыалы фамилия)
+    """
     list_fio = str_fio.split()
     if order == 'fio':
         return f'{list_fio[0]} {list_fio[1][0]}.{list_fio[2][0]}.'
@@ -14,7 +20,12 @@ class MWX:
     def __init__(self) -> None:
         self.book = load_workbook('NBL.xlsx')
 
-    def search_row(self, page_name, list_search=None) -> int:
+    def search_row(self, page_name: str = 'Docs', list_search: list = None) -> int:
+        """
+        Поиск строки удавлетворяющей критериям поиска list_search.
+        Параметры:
+            list_search - список списков [колонка, значение]
+        """
         if list_search is None:
             list_search = list()
         result_search = 0
@@ -28,29 +39,33 @@ class MWX:
                 break
         return result_search
 
-    def giv_const(self, name: str):
+    def giv_const(self, name: str, default: str = None) -> str:
+        """
+        Возвращает значение константы. Константы собраны на странице Const.
+        Параметры:
+            name - содержит имя константы значение которой нужно вернуть (всегда первый столбец).
+            default - возвращаемое значение в случае неудачного поиска. По умолчанию - None
+        """
         name_sheet = 'Const'
         list_search = [[1, name]]
         crow = self.search_row(name_sheet, list_search)
         if crow == 0:
-            return None
+            return default
         else:
             sheet = self.book[name_sheet]
-            return sheet.cell(crow, 2).value
+            return str(sheet.cell(crow, 2).value)
 
-    def get_value(self, page_name='', row=0, col=0) -> str:
-        result = ''
+    def get_value(self, page_name: str = '', row: int = 0, col: int = 0, default: str = '') -> str:
+        value = default
         if page_name == '' or row == 0 or col == 0:
-            return result
+            return value
         try:
             value = self.book[page_name].cell(row, col).value
-        except Exception:
-            value = ''
-        if value is None:
-            value = ''
+        except:
+            pass
         return value
 
-    def get_name(self, page_name: str, list_search=None, default='') -> str:
+    def get_name(self, page_name: str, list_search: list = None, default: str = '') -> str:
         if list_search is None:
             list_search = list()
         row = self.search_row(page_name, list_search)
@@ -61,32 +76,45 @@ class MWX:
             return sheet.cell(row, 2).value
 
     def search_product(self, cod_: str) -> dict:
+        """Осуществляет поиск товара по артикулу и возвращает, в случае успеха,
+        справочник заполненный информацией о товаре. В противном  случае
+        возвращаемый справочник будет пустым."""
         name_sheet = 'product'
         dict_ret = {'код': '', 'наименование': '', 'единица': '', 'ОКЕИ': ''}
         list_search = [[1, cod_]]
         row = self.search_row(name_sheet, list_search)
         if row != 0:
             sheet = self.book[name_sheet]
-            dict_ret['код'] = cod_
-            dict_ret['наименование'] = sheet.cell(row, 2).value
-            dict_ret['единица'] = sheet.cell(row, 3).value
-            dict_ret['ОКЕИ'] = sheet.cell(row, 4).value
+            keys = list(dict_ret.keys())
+            for i in range(4):
+                dict_ret[keys[i]] = sheet.cell(row, i + 1).value
         return dict_ret
 
     def search_nds(self, cod_: str) -> dict:
+        """
+        Осуществляет поиск ставки НДС по коду, в случае успеха, возвращает
+        справочник заполненный информацией о ставке и порядке расчета. В противном
+        случае возвращаемый справочник будет пустым.
+        """
         name_sheet = 'NDS'
-        dict_ret = {'наименование': '', 'ставка': 0, 'ндс_сверху': 0, 'представление': ''}
+        dict_ret = {'наименование': '', 'представление': '', 'ставка': 0, 'ндс_сверху': 0}
         list_search = [[1, cod_]]
         row = self.search_row(name_sheet, list_search)
         if row != 0:
             sheet = self.book[name_sheet]
-            dict_ret['наименование'] = cod_
-            dict_ret['ставка'] = sheet.cell(row, 3).value
-            dict_ret['ндс_сверху'] = sheet.cell(row, 4).value
-            dict_ret['представление'] = sheet.cell(row, 2).value
+            keys = list(dict_ret.keys())
+            for i in range(4):
+                dict_ret[keys[i]] = sheet.cell(row, i + 1).value
         return dict_ret
 
-    def total_customer(self, name_page='', row=0, ret_type='all') -> str:
+    def total_customer(self, name_page: str = '', row: int = 0, ret_type: str = 'all') -> str:
+        """
+        Возвращает форматированную строку с информацией о контрагенте.
+        Парметры:
+            name_page
+            row - номер строки
+            ret_type - тип набора возвращаемых данных.
+        """
         name_page_customer = 'counterparty'
         sheet = self.book[name_page_customer]
 
@@ -118,11 +146,23 @@ class MWX:
                 return f'{name}, {address}'
         return f''
 
-    def test_double(self, name_page, current_row=0, list_test=None):
+    def test_double(self, name_page: str = '', current_row: int = 0, list_test: list = None) -> int:
+        """
+        Производит подсчет строк идентичных заданной.
+        Параметры:
+            name_page - имя стираницы на которой производится подсчет
+            current_row - номер строки, дубли которой считаем
+            list_test - список стобцов по которым проверяем идентичность. Задается
+                паврой - колонка, значение.
+        """
+        count_double = 0
+        if name_page == '':
+            return count_double
+        sheet = self.book[name_page]
+
         if list_test is None:
             list_test = list()
-        sheet = self.book[name_page]
-        count_double = 0
+
         for row in range(2, sheet.max_row + 1):
             flag = True
             for test_col in list_test:
@@ -132,7 +172,10 @@ class MWX:
                     count_double += 1
         return count_double
 
-    def transform(self, sheet_in, row: int, name_page="", default='', tmpl='') -> str:
+    def transform(self, sheet_in: str, row: int, name_page: str = "", default: str = '', tmpl: str = '') -> str:
+        """
+        Функция возвращает преобразованное значение.
+        """
         result = default
         if name_page == 'counterparty':
             sheet = self.book[sheet_in]
@@ -141,12 +184,12 @@ class MWX:
 
         return f'{result:{tmpl}}'
 
-    def get_str_list(self, name_list='Docs', rows=None, columns=None) -> list:
+    def get_str_list(self, name_list='Docs', rows: list = None, columns: list = None) -> list:
         if columns is None:
             columns = list()
         s_page = self.book[name_list]
         if rows is None:
-            rows = range(2, s_page.max_row + 1)
+            rows = [x for x in range(2, s_page.max_row + 1)]
         list_docs = [tuple([self.transform(name_list, crow, col['trans'],
                                            self.get_value(name_list, crow, col['position']), col['format'])
                             for col in columns]) for crow in rows]
@@ -159,15 +202,20 @@ class MWX:
         if row == 0:
             row = sheet.max_row + 1
         for column in range(len(list_data)):
-            sheet.cell(row, column+1).value = list_data[column]
+            sheet.cell(row, column + 1).value = list_data[column]
         self.book.save('NBL.xlsx')
 
-    def del_row(self, name_sheet,  row):
+    def del_row(self, name_sheet, row):
         sheet = self.book[name_sheet]
         sheet.delete_rows(row)
         self.save_list(name_sheet)
 
-    def prn_t12(self, row_print: int):
+    def prn_t12(self, row_print: int) -> None:
+        """
+        Вывод документа по форме T12
+        Параметры:
+            row_print - номер строки документа из закладки Docs
+        """
         page = self.book['Docs']
 
         template_sf = load_workbook('торг12.xlsx')
@@ -180,8 +228,8 @@ class MWX:
         target_page.cell(8, 4).value = customer
         target_page.cell(12, 4).value = customer
         target_page.cell(13, 42).value = f'{page.cell(row_print, 17).value}'
-        target_page.cell(15, 42).value = f'{page.cell(row_print,18).value}'
-        target_page.cell(14, 4).value = f'{page.cell(row_print,17).value} от {page.cell(row_print,18).value}'
+        target_page.cell(15, 42).value = f'{page.cell(row_print, 18).value}'
+        target_page.cell(14, 4).value = f'{page.cell(row_print, 17).value} от {page.cell(row_print, 18).value}'
         target_page.cell(17, 12).value = page.cell(row_print, 1).value
         target_page.cell(17, 16).value = page.cell(row_print, 4).value
         target_page.cell(17, 16).number_format = 'DD.MM.YYYY'
@@ -200,13 +248,13 @@ class MWX:
         target_page.cell(23, 34).value = dic_nds['представление']
         if dic_nds['ндс_сверху'] == 1:
             sum_bez_nds = float(quantity) * float(cost)
-            nds = dic_nds['ставка']/100
+            nds = dic_nds['ставка'] / 100
             target_page.cell(23, 29).value = sum_bez_nds
             target_page.cell(23, 38).value = sum_bez_nds * nds
             target_page.cell(23, 41).value = sum_bez_nds * (1 + nds)
         else:
             sum_s_nds = quantity * cost
-            nds = dic_nds['ставка']/100
+            nds = dic_nds['ставка'] / 100
             target_page.cell(23, 29).value = sum_s_nds / (1 + nds)
             target_page.cell(23, 38).value = sum_s_nds * nds / (1 + nds)
             target_page.cell(23, 41).value = sum_s_nds
@@ -214,9 +262,14 @@ class MWX:
         target_page.cell(45, 8).value = page.cell(row_print, 4).value
 
         path = f'{os.getcwd()}\\docs\\'
-        template_sf.save(f'{path}{str(page.cell(row_print,1).value)}_t12.xlsx')
+        template_sf.save(f'{path}{str(page.cell(row_print, 1).value)}_t12.xlsx')
 
-    def prn_p4(self, row_print: int):
+    def prn_p4(self, row_print: int) -> None:
+        """
+        Вывод документа по форме P4
+        :param row_print: число - номер строки из листа Docs
+        :return: None
+        """
         s_page = self.book['Docs']
 
         template_sf = load_workbook('Приложение4.xlsx')
@@ -302,9 +355,14 @@ class MWX:
         target_page.cell(84, 28).value = f'{s_page.cell(row_print, 9).value}'
 
         path = f'{os.getcwd()}\\docs\\'
-        template_sf.save(f'{path}{str(s_page.cell(row_print,1).value)}_p4.xlsx')
+        template_sf.save(f'{path}{str(s_page.cell(row_print, 1).value)}_p4.xlsx')
 
-    def prn_sf(self, row_print):
+    def prn_sf(self, row_print: int) -> None:
+        """
+        Выводит документ СЧЕТ-ФАКТУРУ
+        :param row_print: число номер строки выводимого документа из листа Docs
+        :return: None
+        """
         s_page = self.book['Docs']
 
         dic_str = self.search_product(str(s_page.cell(row_print, 14).value))
@@ -342,13 +400,13 @@ class MWX:
         target_page.cell(15, 30).value = dic_nds['представление']
         if dic_nds['ндс_сверху'] == 1:
             sum_bez_nds = float(quantity) * float(cost)
-            nds = dic_nds['ставка']/100
+            nds = dic_nds['ставка'] / 100
             target_page.cell(15, 23).value = sum_bez_nds
             target_page.cell(15, 33).value = sum_bez_nds * nds
             target_page.cell(15, 36).value = sum_bez_nds * (1 + nds)
         else:
             sum_s_nds = quantity * cost
-            nds = dic_nds['ставка']/100
+            nds = dic_nds['ставка'] / 100
             target_page.cell(15, 23).value = sum_s_nds / (1 + nds)
             target_page.cell(15, 33).value = sum_s_nds * nds / (1 + nds)
             target_page.cell(15, 36).value = sum_s_nds
@@ -363,7 +421,8 @@ class MWX:
 if __name__ == '__main__':
     row_doc_print = 2
     wb = MWX()
-
-    wb.prn_t12(row_doc_print)
-    wb.prn_p4(row_doc_print)
-    wb.prn_sf(row_doc_print)
+    print(wb.get_str_list(
+        name_list='Docs',
+        columns=[{'position': 1, 'format': '', 'trans': '', 'width': 50},
+                 {'position': 4, 'format': '', 'trans': '', 'width': 75},
+                 {'position': 3, 'format': '', 'trans': 'counterparty', 'width': 300}]))
